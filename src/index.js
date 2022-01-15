416aafcaf667fba861b83df81e58db567279d8fa
tuananh01011994 <43560681+tuananh01011994@users.noreply.github.com>
2022-01-15 22:49:45 +0700

    up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap-icons/font/bootstrap-icons.css";
+
 import axios from "axios";
 
 ReactDOM.render(
